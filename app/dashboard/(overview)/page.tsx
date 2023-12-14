5db9419cd9e67cd1e99152228d8c407226e9c7c2
Muhammad Hany <dev.mhany@gmail.com>
2023-12-15 01:22:31 +0200

    mutate data using forms and React Server Actions.

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,7 +1,7 @@
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchCardData } from '@/app/lib/data'; // Remove fetchLatestInvoices
+import { fetchCardData } from '@/app/lib/data';
 import CardWrapper from '@/app/ui/dashboard/cards';
 import { Suspense } from 'react';
 import {
@@ -9,6 +9,7 @@ import {
   LatestInvoicesSkeleton,
   CardsSkeleton,
 } from '@/app/ui/skeletons';
+import { Card } from '@/app/ui/dashboard/cards';
 
 export default async function Page() {
   return (
@@ -19,6 +20,14 @@ export default async function Page() {
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
+        </Suspense>
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
         </Suspense>
       </div>
     </main>
