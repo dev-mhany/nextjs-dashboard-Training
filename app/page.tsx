93673eac96ddf4a434d376f6339e4295daaf0293
Muhammad Hany <dev.mhany@gmail.com>
2023-12-14 15:08:46 +0200

    added some page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -38,6 +38,15 @@ export default function Page() {
             height={760}
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop version"
+            priority
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing mobile version"
+            priority
           />
         </div>
       </div>
