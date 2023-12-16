71eb039a10d759eae8e1fbbaa00a40c78169d22b
Muhammad Hany <dev.mhany@gmail.com>
2023-12-16 16:59:11 +0200

    meta added

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,8 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'customers | Acme Dashboard',
+};
 export default function Page() {
   return <p>Customers Page</p>;
 }
